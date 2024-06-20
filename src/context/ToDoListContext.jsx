7181e2c3c644f25f66d1d1149f1827ae41a3a744
Navidul Hoque <64648003+NavidulHoque@users.noreply.github.com>
2024-06-20 18:47:25 +0600

    created a todolist

--- src/context/ToDoListContext.jsx
+++ src/context/ToDoListContext.jsx
@@ -5,7 +5,7 @@ export const ToDoListContext = createContext([])
 
 export const ToDoListProvider = ({children}) => {
 
-    //these states are used in App.jsx
+    //these states are declared in App.jsx
     const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("todoList")) || []) //here all the todo list will be stored
     const [recentTodoItem, setRecentTodoItem] = useState(JSON.parse(localStorage.getItem("todoItem")) || "") //will handle the recent todo being written by the user
     const [showFinished, setshowFinished] = useState(true)
